2cfcb35aa8dd4fe36368aa7662875794cc71e62f
Jonas Kahnwald <115373601+JOHN-72@users.noreply.github.com>
2022-10-17 00:39:47 +0530

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -1,41 +1,33 @@
-// C++ program to implement iterative Binary Search
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
- 
-// A iterative binary search function. It returns
-// location of x in given array arr[l..r] if present,
-// otherwise -1
-int binarySearch(int arr[], int l, int r, int x)
+int main()
 {
-    while (l <= r) {
-        int m = l + (r - l) / 2;
- 
-        // Check if x is present at mid
-        if (arr[m] == x)
-            return m;
- 
-        // If x greater, ignore left half
-        if (arr[m] < x)
-            l = m + 1;
- 
-        // If x is smaller, ignore right half
-        else
-            r = m - 1;
+   int myarray[10] = {21,43,23,54,75,13,5,8,25,10};  
+    int key,loc;
+    cout<<"The input array is"<<endl;
+    for(int i=0;i<10;i++){
+        cout<<myarray[i]<<" ";
     }
- 
-    // if we reach here, then element was
-    // not present
-    return -1;
-}
- 
-int main(void)
-{
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int result = binarySearch(arr, 0, n - 1, x);
-    (result == -1)
-        ? cout << "Element is not present in array"
-        : cout << "Element is present at index " << result;
-    return 0;
+    cout<<endl;
+    cout<<"Enter the key to be searched : "; cin>>key;
+    for (int i = 0; i< 10; i++)  
+    {  
+        if(myarray[i] == key)   
+        {  
+            loc = i+1;
+            break;  
+        }   
+        else  
+        loc = 0;  
+    }   
+    if(loc != 0)  
+    {  
+        cout<<"Key found at position "<<loc<<" in the array";  
+    }  
+    else 
+    {  
+        cout<<"Could not find given key in the array";  
+    }  
+   
 }
